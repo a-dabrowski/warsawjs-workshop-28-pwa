0971d7ee4a6020ee1fe37968c05c94679972f31a
Adam Dąbrowski <adamdabrowski@adabrowski-macbook-air-547.local>
2019-01-13 15:52:06 +0100

    Init caching

--- client/src/sw/index.js
+++ client/src/sw/index.js
@@ -1,3 +1,26 @@
 import '@babel/polyfill';
 
 console.log('Hello worker');
+
+
+const PRECACHE = 'mycache-1';
+
+const PRECACHE_URLS = [
+  '/assets/manifest.json',
+  '/assets/icon_192x192.714a1246445a6a0da1e3aadf1451234a.png',
+  // application assets
+  '/',
+  '/assets/index.js',
+];
+
+
+async function precache(urls) {
+  const cache = await caches.open(PRECACHE);
+  return cache.addAll(urls);
+}
+
+
+self.addEventListener('install', (event) => { // eslint-disable-line no-restricted-globals
+  console.log('inside install');
+  event.waitUntil(precache(PRECACHE_URLS));
+});
